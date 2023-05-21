ac1603af41776613ec04e488c5f07b64180d46c5
BeraTr1 <beratr.alias@gmail.com>
2023-05-21 03:16:45 -0600

    Update reminder.js

--- bot_modules/reminder.js
+++ bot_modules/reminder.js
@@ -10,9 +10,10 @@ exports.setReminder = (client, msg) => {
     // check if message was sent by the bot
     if (msg.author.id === client.user.id) return;
     
-    let prefix = "?reminder"
+    // let prefix = "?reminder"
 
-    if (msg.content.startsWith("?")){
-        msg.reply("test");
-    }
+    // if (msg.content.startsWith("?")){
+    //     msg.reply("test");
+    // }
+    console.log("test");
 }
