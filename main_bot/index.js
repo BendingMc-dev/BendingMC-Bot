85356ed3e678a45c4ec6f78e30c96a568a0b72f9
BeraTr1 <beratr.alias@gmail.com>
2023-05-27 10:39:13 -0600

    implemented 2 separate bots

---
+++ main_bot/index.js
@@ -0,0 +1,52 @@
+const { Client, IntentsBitField, Partials, Events, Channel } = require('discord.js');
+
+const suggestions = require("./bot_modules/suggestions.js");
+// const reminder = require("./bot_modules/reminder.js");
+const todo = require("./bot_modules/todo.js");
+
+require('dotenv').config();
+
+const client = new Client({
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.MessageContent,
+		IntentsBitField.Flags.GuildMessageReactions
+	],
+	partials: [
+		Partials.Message,
+		Partials.Channel,
+		Partials.Reaction
+	]
+});
+
+// Debug message event listener
+client.on(Events.MessageCreate, msg =>{
+	if (msg.author.id === client.user.id){
+		return
+	}
+	
+	if (msg.content === "ping"){
+		msg.reply("pong!");
+	}
+})
+
+// Execute modules based on events
+//FIXME implement event manager
+suggestions.resolveSuggestion(client, Events); //FIXME use the function after the event is triggered instead -> "on new message, function()"
+// todo.newTodo(client, Events);
+
+client.on(Events.MessageCreate, msg =>{
+	suggestions.onNewSuggestion(client, msg);
+	// reminder.setReminder(client, msg);
+})
+
+// Log in
+const BOT_TOKEN = process.env.BOT_TOKEN;
+client.login(BOT_TOKEN);
+
+// Start console message
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
