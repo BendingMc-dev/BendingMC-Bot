90880982fcf76ce497822b44521421bb74bc610b
BeraTr1 <beratr.alias@gmail.com>
2023-05-28 03:11:46 -0600

    Update todo.js

--- test_bot/bot_modules/todo.js
+++ test_bot/bot_modules/todo.js
@@ -98,49 +98,49 @@ exports.newTodo = (client, Events) => {
             // tableColumns += `${key} ${value}` (? :) 
         })
 
-        mysql.createTable(database.table, tableColumns);
+        mysql.createTable(database.table, tableColumns).then( ()=>{
+            createDBEntry(channelId).then(() =>{
+                // fetch channel entry from database as 'entry'
+                mysql.fetch(channelId, database.table).then((entry) =>{ //FIXME put into function (add/display todo)
+                    // console.log("Channel entry: " + entry) //DEBUG
+                    // console.log("Channel entry[0].Id: " + entry[0].Id) //DEBUG
+                    
+                    // let channelTodo = entry[0].Todo;
+                    // let content = (msg.content.split(prefix + " ")[1] === undefined ? msg.content.split(prefix)[1] : msg.content.split(prefix + " ")[1]);
+                    // // let regex = new RegExp("(\', \", \`)");
 
-        createDBEntry(channelId).then(() =>{
-            // fetch channel entry from database as 'entry'
-            mysql.fetch(channelId, database.table).then((entry) =>{ //FIXME put into function (add/display todo)
-                // console.log("Channel entry: " + entry) //DEBUG
-                // console.log("Channel entry[0].Id: " + entry[0].Id) //DEBUG
-                
-                // let channelTodo = entry[0].Todo;
-                // let content = (msg.content.split(prefix + " ")[1] === undefined ? msg.content.split(prefix)[1] : msg.content.split(prefix + " ")[1]);
-                // // let regex = new RegExp("(\', \", \`)");
+                    // content = content.replaceAll("\\", "\\\\");
+                    // content = content.replaceAll("\"", "\\\"");
+                    // console.log("Message is: (" + content + ")"); //DEBUG
 
-                // content = content.replaceAll("\\", "\\\\");
-                // content = content.replaceAll("\"", "\\\"");
-                // console.log("Message is: (" + content + ")"); //DEBUG
+                    // // check if message content exists after removing prefix
+                    // if (!content) {
+                    //     //FIXME display todo of channel
+                    //     console.log("Todo command has no content. Displaying todo of channel."); //DEBUG
+                    //     console.log("Channel todo: " + channelTodo); //DEBUG
+                    // } else {
+                    //     //FIXME add todo to channel
+                    //     console.log("Todo command has content. Adding new todo item to channel") //DEBUG
+                    //     channelTodo += `"${content}",`;
+                    //     mysql.update(database.table, "Todo", channelTodo, channelId);
+                    // }
 
-                // // check if message content exists after removing prefix
-                // if (!content) {
-                //     //FIXME display todo of channel
-                //     console.log("Todo command has no content. Displaying todo of channel."); //DEBUG
-                //     console.log("Channel todo: " + channelTodo); //DEBUG
-                // } else {
-                //     //FIXME add todo to channel
-                //     console.log("Todo command has content. Adding new todo item to channel") //DEBUG
-                //     channelTodo += `"${content}",`;
-                //     mysql.update(database.table, "Todo", channelTodo, channelId);
-                // }
+                    let messageContent = (msg.content.split(prefix + " ")[1] === undefined ? msg.content.split(prefix)[1] : msg.content.split(prefix + " ")[1]);
 
-                let messageContent = (msg.content.split(prefix + " ")[1] === undefined ? msg.content.split(prefix)[1] : msg.content.split(prefix + " ")[1]);
+                    if (!messageContent){
+                        //FIXME display todo of channel
+                        console.log("message doesn't have any todo items. Displaying channel todo: " + entry[0].Todo); //DEBUG
+                    } else {
+                        //FIXME add todo item to channel
+                        console.log("message has todo item. Adding item to the database"); //DEBUG
+                        let todoItem = new TodoItem(1, messageContent);
+                        console.log(JSON.stringify(todoItem)); //DEBUG
+                    }
 
-                if (!messageContent){
-                    //FIXME display todo of channel
-                    console.log("message doesn't have any todo items. Displaying channel todo: " + entry[0].Todo); //DEBUG
-                } else {
-                    //FIXME add todo item to channel
-                    console.log("message has todo item. Adding item to the database"); //DEBUG
-                    let todoItem = new TodoItem(1, messageContent);
-                    console.log(JSON.stringify(todoItem)); //DEBUG
-                }
-
-                //FIXME add a way to remove todo item (at the start of each todo item, add a number as an id)
-                //FIXME create embed manager
-            });
+                    //FIXME add a way to remove todo item (at the start of each todo item, add a number as an id)
+                    //FIXME create embed manager
+                });
+            })
         })
     })
 }
