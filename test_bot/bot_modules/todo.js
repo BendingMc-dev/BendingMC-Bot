74e83632af60b6792d84f15872c042828a807a8a
BeraTr1 <beratr.alias@gmail.com>
2023-05-30 17:41:28 -0600

    Update todo.js

--- test_bot/bot_modules/todo.js
+++ test_bot/bot_modules/todo.js
@@ -113,6 +113,10 @@ function displayTodo(channelId, channelName){
             },
             {
                 name: '',
+                value: '──────────────────────────────────────\n',
+            },
+            {
+                name: '',
                 value: '┌──────────────────────────────────────\n',
             },
             {
@@ -134,9 +138,11 @@ function displayTodo(channelId, channelName){
         let capitalizedTodoItemContent = todoItem.content.charAt(0).toUpperCase() + todoItem.content.slice(1);
 
         messageResponseEmbed.fields[0].value +=  `☐ │ ${todoItem.count}. ${capitalizedTodoItemContent} ${(todoItem.author ? '(<@' + todoItem.author + '>)' : '')} \n\n`;
+
+        messageResponseEmbed.fields[1].value +=  `${todoItem.count}. ${capitalizedTodoItemContent} ${(todoItem.author ? '(<@' + todoItem.author + '>)' : '')} \n\n`;
         
-        messageResponseEmbed.fields[1].value +=  `  │ ${todoItem.count}. ${capitalizedTodoItemContent} ${(todoItem.author ? '(<@' + todoItem.author + '>)' : '')} \n`;
-        messageResponseEmbed.fields[1].value +=  `  │\n`;
+        messageResponseEmbed.fields[2].value +=  `  │ ${todoItem.count}. ${capitalizedTodoItemContent} ${(todoItem.author ? '(<@' + todoItem.author + '>)' : '')} \n`;
+        messageResponseEmbed.fields[2].value +=  `  │\n`;
     }
 
     // console.log("Message response to displayTodo command: " + messageResponse); //DEBUG
