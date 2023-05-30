e5e3e1ce65f785ce69a2edb2ffdbafdb6c5d2046
BeraTr1 <beratr.alias@gmail.com>
2023-05-29 18:16:41 -0600

    Update todo.js

--- test_bot/bot_modules/todo.js
+++ test_bot/bot_modules/todo.js
@@ -31,8 +31,8 @@ const database = { //FIXME create a new class in another file to handle database
 }
 
 const files = {
-    mainPath: __dirname + "/data/todo-lists/"
-    // mainPath: "data/"
+    // mainPath: __dirname + "/data/todo-lists/"
+    mainPath: "data/"
 }
 
 class TodoItem {
