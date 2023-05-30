9766794a77fac31ad5ee2741b2082b1ae6feb1e4
BeraTr1 <beratr.alias@gmail.com>
2023-05-29 18:36:18 -0600

    Update json-manager.js

--- test_bot/utils/json-manager.js
+++ test_bot/utils/json-manager.js
@@ -13,22 +13,22 @@ exports.makeDir = (dir) => {
     fs.mkdirSync(dir, {recursive: true}, (err) =>{
         if (!err) return;
 
-        console.error("There was an error while making a directory");
-        console.error(err);
+        console.log("There was an error while making a directory");
+        console.log(err);
     });
 
     console.log(`New directory created: ${dir}`)
 }
 
 exports.saveFile = (file, data = "") =>{
-    const dirname = path.dirname(file);
+    // const dirname = path.dirname(file);
 
-    if (!this.fileExists(dirname))
-        this.makeDir(dirname);
+    // if (!this.fileExists(dirname))
+    //     this.makeDir(dirname);
 
     fs.writeFile(file, data, (err) =>{
         if (!err) return;
-        
+
         console.log("There was an error while saving a json file");
         console.log(err);
     });
