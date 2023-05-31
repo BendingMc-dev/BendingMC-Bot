e49822c8a038714f2967ba4d1b5bde341e906e8f
BeraTr1 <beratr.alias@gmail.com>
2023-05-30 19:08:37 -0600

    Update todo.js

--- test_bot/bot_modules/todo.js
+++ test_bot/bot_modules/todo.js
@@ -58,26 +58,9 @@ function saveTodoList(channelId, todoItems){
 function newTodo(channelId, messageContent, messageAuthor){
     const filePath = getFilePath(channelId);
     let fileTodoItems = fs.readFile(filePath);
-
-    // console.log("Message has todo item. Saving todo in file"); //DEBUG
-
-    // const todoList = new TodoList();
-
-    // for (let fileTodoItem of fileTodoItems.todo){
-    //     const todoItem = new TodoItem(fileTodoItem.count, fileTodoItem.content);
-
-    //     todoList.todoList.push(todoItem);
-    // }
-
     let todoListCount = fileTodoItems.todo.length + 1;
-    // const todoItem = new TodoItem(todoListCount, messageContent);
-
-    // todoList.todoList.push(todoItem);
-
-    // const jsonTodoList = JSON.stringify(todoList, null, 2);
-
-    // fs.saveFile(filePath, jsonTodoList);
     const todoItem = new TodoItem(todoListCount, messageContent, messageAuthor);
+
     fileTodoItems.todo.push(todoItem);
 
     saveTodoList(channelId, fileTodoItems.todo);
@@ -91,35 +74,19 @@ function displayTodo(channelId, channelName){
     const filePath = getFilePath(channelId);
     const fileTodoItems = fs.readFile(filePath);
 
-    // console.log("Message does not have todo item. Displaying todo list of channel"); //DEBUG
-    // for (let todoItem of fileTodoItems.todo){
-    //     console.log("Todo item of channel: " + todoItem.count + " -> " + todoItem.content);
-    // }
-
-    // let messageResponse = `WIP. Displaying todo item of channel:\n`;
     const messageResponseEmbed = {
         color: 0xFB4E88,
-        // title: '',
-        // url: '', //'https://discord.js.org',
         author: {
-            name: `Channel Todo List`, //'Some name',
+            name: `Channel Todo List`,
             icon_url: 'https:\/\/www.dropbox.com\/temp_thumb_from_token\/s\/u5ajixq3x3ubmsp?preserve_transparency=False&size=1200x1200&size_mode=4',
-            // url: '', //'https://discord.js.org',
         },
-        description: '```yml\n' + channelName + '\n```', //'Some description here',
+        description: '```markdown\n' + channelName + '\n```',
         fields: [
             {
                 name: '',
                 value: '',
             },
         ],
-        // image: {
-        //     url: '', //'https://i.imgur.com/AfFp7pu.png',
-        // },
-        footer: {
-            text: '', //'Some footer text here',
-            icon_url: '', //'https://i.imgur.com/AfFp7pu.png',
-        },
     };
 
     messageResponseEmbed.fields[0].value += "```markdown\n";
@@ -128,7 +95,7 @@ function displayTodo(channelId, channelName){
         messageResponseEmbed.fields[0].value += "It looks like this channel doesn't have any todo items!\n";
     } else {
         //FIXME add limit (if the next item + the current items length is more than 1000, create a new field and start adding there instead)
-        
+
         for (let todoItem of fileTodoItems.todo){
             let capitalizedTodoItemContent = todoItem.content.charAt(0).toUpperCase() + todoItem.content.slice(1);
 
@@ -139,7 +106,6 @@ function displayTodo(channelId, channelName){
 
     messageResponseEmbed.fields[0].value += "```";
 
-    // console.log("Message response to displayTodo command: " + messageResponse); //DEBUG
     return {embeds: [messageResponseEmbed]};
 }
 
@@ -148,7 +114,6 @@ function removeTodo(channelId, todoNumber){
     const fileTodoItems = fs.readFile(filePath);
 
     const filteredTodoItems = fileTodoItems.todo.filter(todoItem =>{
-        // console.log(`Filtering items... Count of todoItem (${todoItem.count}) and number of item to be removed ${todoNumber}`); //DEBUG
         return todoItem.count != todoNumber;
     });
     
@@ -173,9 +138,8 @@ exports.onTodoCommand = (client, Events) =>{
         // check if message has prefix
         if (!msg.content.startsWith(prefix)) return;
 
-        // if (!msg.member.roles.cache.has(requiredRole)) return;
-
-        console.log("--------- Start of Log ---------"); //DEBUG
+        // check if user has the "staff" role (bmc)
+        if (!msg.member.roles.cache.has(requiredRole)) return;
 
         const channelId = msg.channel.id;
         const filePath = getFilePath(channelId);
@@ -187,7 +151,6 @@ exports.onTodoCommand = (client, Events) =>{
 
             const todoList = new TodoList();
             let jsonTodoList = JSON.stringify(todoList, null, 2);
-            // console.log("New file created with the json: " + jsonTodoList); //DEBUG
 
             fs.saveFile(filePath, jsonTodoList);
         }
@@ -223,19 +186,5 @@ exports.onTodoCommand = (client, Events) =>{
             return;
             
         msg.reply(response);
-
-        // check if message exists
-        // let messageContent = (msg.content.split(prefix + " ")[1] === undefined ? msg.content.split(prefix)[1] : msg.content.split(prefix + " ")[1]);
-
-        // const fileTodoItems = fs.readFile(filePath);
-
-        // if (messageContent){
-        //     newTodo(fileTodoItems, messageContent, filePath);
-        // } else {
-        //     displayTodo(fileTodoItems);
-        // }
-
-        console.log("--------- End of Log ---------"); //DEBUG
-        console.log(" "); //DEBUG
     })    
 }
