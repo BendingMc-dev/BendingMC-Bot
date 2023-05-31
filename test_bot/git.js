edde4f27bf1d7d19677b0c06877865cb8b76b0f5
BeraTr1 <beratr.alias@gmail.com>
2023-05-31 17:54:52 -0600

    Update git.js

--- test_bot/git.js
+++ test_bot/git.js
@@ -9,7 +9,7 @@ const TOKEN = process.env.GIT_AUTH_TOKEN;
 exports.cloneRepo = () =>{
     const REPO = `github.com/${USER}/Screeps`;
 
-    const remote = `https://${USER}:${PASS}@${REPO}`;
+    const remote = `https://${USER}:${TOKEN}@${REPO}`;
 
     git.clone(remote, process.cwd() + '/test/')
         .then(() => console.log('finished'))
