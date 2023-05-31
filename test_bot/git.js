e5ca76d138508eb51018c1da1eedd0aa055b11b7
BeraTr1 <beratr.alias@gmail.com>
2023-05-31 17:42:48 -0600

    Update git.js

--- test_bot/git.js
+++ test_bot/git.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 const simpleGit = require('simple-git');
 
-const git = simpleGit('/test/');
+const git = simpleGit();
 const USER = process.env.GIT_USER;
 const PASS = process.env.GIT_PASSWORD;
 
 exports.cloneRepo = () =>{
-    const REPO = 'github.com/username/private-repo';
+    const REPO = `github.com/${USER}/Screeps`;
 
     const remote = `https://${USER}:${PASS}@${REPO}`;
 
-    git.clone(remote)
+    git.clone(remote, '/test')
         .then(() => console.log('finished'))
         .catch((err) => console.error('failed: ', err));
 }
