55fbbb9215e94de6ca03229b55954110901fc431
BeraTr1 <beratr.alias@gmail.com>
2023-05-31 18:02:11 -0600

    Update git.js

--- test_bot/git.js
+++ test_bot/git.js
@@ -1,17 +1,21 @@
 require('dotenv').config();
-
 const simpleGit = require('simple-git');
 
 const git = simpleGit();
-const USER = process.env.GIT_USER;
-const TOKEN = process.env.GIT_AUTH_TOKEN;
+
+const config = {
+    auth_user: process.env.GIT_USER,
+    auth_token: process.env.GIT_AUTH_TOKEN,
+    repo: "/Screeps"
+}
 
 exports.cloneRepo = () =>{
-    const REPO = `github.com/${USER}/Screeps`;
+    const REPO = `github.com/${config.auth_user}/${config.repo}`;
+    const remote = `https://${config.auth_user}:${config.auth_token}@${REPO}`;
 
-    const remote = `https://${USER}:${TOKEN}@${REPO}`;
+    console.log('Attempting to clone git repository...');
 
     git.clone(remote, process.cwd() + '/test/')
-        .then(() => console.log('finished'))
-        .catch((err) => console.error('failed: ', err));
+        .then(() => console.log('Finished cloning git repository!'))
+        .catch((err) => console.error('There was an error while cloning git repository: ', err));
 }
