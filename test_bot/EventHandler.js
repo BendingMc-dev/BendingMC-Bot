2dcb47cd8e1c68a27e51f2aa6fc82c392c24a10b
BeraTr1 <beratr.alias@gmail.com>
2023-08-22 21:59:31 -0600

    Create EventHandler.js

---
+++ test_bot/EventHandler.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const fs = require("fs");
+
+module.exports = function main(client) {
+    // get all files in a folder
+    // for each file, require a function inside of it
+    
+    let folderPath = path.join(__dirname, "bot_modules");
+
+    console.log("folder path: " + folderPath);
+
+    let files = fs.readdirSync(folderPath, {withFileTypes: true});
+
+    for (let file of files) {
+        console.log("name of file: " + file.name);
+    }
+}
