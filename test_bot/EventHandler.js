de6c36c4680fa8eb5798ba5fae9a0447fd610114
BeraTr1 <beratr.alias@gmail.com>
2023-08-22 22:24:14 -0600

    Update EventHandler.js

--- test_bot/EventHandler.js
+++ test_bot/EventHandler.js
@@ -1,18 +1,25 @@
 const path = require("path");
 const fs = require("fs");
 
-function getFiles(path) {
+function getFiles(directory, foldersOnly = false) {
     let files = [];
-    let dirents = fs.readdirSync(path, {withFileTypes: true});
+    let dirents = fs.readdirSync(directory, {withFileTypes: true});
 
     for (let dirent of dirents) {
-        console.log("(all) file name: " + dirent.name);
+        let filePath = path.join(directory, dirent.name);
 
-        // check if dirent is file
-        let isFile = dirent.isFile();
-        if (!isFile) continue;
+        // if folders only is true, this function will only return folders. Otherwise, it will only return file paths
+        if (foldersOnly) {
+            let isFolder = dirent.isDirectory();
+            if (!isFolder) continue;
 
-        files.push(dirent);
+            files.push(filePath);
+        } else {
+            let isFile = dirent.isFile();
+            if (!isFile) continue;
+
+            files.push(filePath);
+        }
     }
 
     return files;
@@ -22,7 +29,7 @@ module.exports = (client) => {
     // get all files in a folder
     // for each file, require a function inside of it
     
-    let folderPath = path.join(__dirname, "bot_modules");
+    const folderPath = path.join(__dirname, "bot_modules");
 
     console.log("folder path: " + folderPath);
 
@@ -30,5 +37,6 @@ module.exports = (client) => {
 
     for (let file of files) {
         // console.log("file name: " + file.name);
+        console.log("found file: " + file);
     }
 }
