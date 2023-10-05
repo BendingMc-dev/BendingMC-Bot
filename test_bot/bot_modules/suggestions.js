04d36b42179ebf2db32e0605bb945d47bf0d1746
BeraTr1 <beratr.alias@gmail.com>
2023-10-05 13:26:20 -0600

    cc

--- test_bot/bot_modules/suggestions.js
+++ test_bot/bot_modules/suggestions.js
@@ -172,11 +172,10 @@ exports.resolveSuggestion = (client, Events) =>{
 
         // Send message and close post
         let owner = await channel.fetchOwner();
-        let ownerIsNull = owner == null;
 
-        if (ownerIsNull) return;
-
-        let message = `Hello <@${owner.id}>! This suggestion has been ${decisionTag.name.toLowerCase()} by <@${user.id}>! If you have any questions regarding the decision, please contact <@${user.id}>. This post has been locked and closed.`;
+        let messageOwnerNotNull = `Hello <@${owner.id}>! This suggestion has been ${decisionTag.name} by <@${user.id}>! If you have any questions regarding the decision, please contact <@${user.id}>. This post has been locked and closed.`;
+        let messageOwnerIsNull = `Hello! This suggestion has been ${decisionTag.name} by <@${user.id}>! If you have any questions regarding the decision, please contact <@${user.id}>. This post has been locked and closed.`;
+        let message = owner == null ? messageOwnerIsNull : messageOwnerNotNull;
         
         await channel.send(message);
         
